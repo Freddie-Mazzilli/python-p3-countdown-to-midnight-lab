d14a943fe70cbf1e1ac558ffd0f482c2ba892969
Freddie Mazzilli <freddie.mazzilli@gmail.com>
2023-05-25 12:31:12 -0400

    Completes countdown to midnight lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,20 @@
+import time
 # your code goes here!
+def countdown(self):
+    while self >= 0 :
+        if self > 0:
+            print(f'{self} SECOND(S)!')
+            self -= 1
+        elif self == 0:
+            print('HAPPY NEW YEAR!')
+            self -=1
+
+def countdown_with_sleep(self):
+    while self >= 0 :
+        time.sleep(1)
+        if self > 0:
+            print(f'{self} SECOND(S)!')
+            self -= 1
+        elif self == 0:
+            print('HAPPY NEW YEAR!')
+            self -=1
